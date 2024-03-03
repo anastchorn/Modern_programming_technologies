cb7f9b78c1a09528adeb9d876bd470ad94d08823
anastchorn <146370239+anastchorn@users.noreply.github.com>
2024-03-04 00:47:49 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,8 +1,16 @@
 # Функція сортування бульбашкою
-def sort_list(arr):
+def bubble_sort(arr):
+    
     n = len(arr)
     for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+
+if __name__ == "__main__":
+    unsorted_list = [64, 34, 25, 12, 22, 11, 90]
+    sorted_list = bubble_sort(unsorted_list)
+    print("Отсортований список:", sorted_list)
