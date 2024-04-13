426c463ac7cfb317be2d7304cadcf1b35975efc1
anastchorn <anastchorn-ipt27@lll.kpi.ua>
2024-04-13 17:29:47 +0300

    second commit

--- Lab1_rot13/Lab1_rot.py
+++ Lab1_rot13/Lab1_rot.py
@@ -15,13 +15,12 @@ def rot13(text):
 def main():
      # функція main читає вхідні дані, застосовує шифрування і виводить результат. Також обробляє помилки та виводить їх у стандартний потік помилок
     try:
-        sys.stdout.write("Введіть текст для шифрування та натисніть Enter: ")
         sys.stdout.flush()
         input_text = sys.stdin.readline().strip()
         if not input_text.isascii() or any(char.isdigit() for char in input_text):
             raise ValueError("Вхідні данні можуть містити тільки ASCII символи.")
         encrypted_text = rot13(input_text)
-        sys.stdout.write("Зашифрований текст: " + encrypted_text + '\n')
+        sys.stdout.write(encrypted_text)
     except Exception as e:
         sys.stderr.write("Помилка: {}\n".format(e))
         sys.exit(1)
--- Lab1_rot13/test_Lab1_rot.py
+++ Lab1_rot13/test_Lab1_rot.py
@@ -3,6 +3,7 @@ from io import StringIO
 import sys
 from Lab1_rot import rot13, main
 
+
 class TestRot13Cipher(unittest.TestCase):
     def setUp(self):
         self.input_text = 'Hello, World!'
@@ -18,40 +19,59 @@ class TestRot13Cipher(unittest.TestCase):
         # тест тільки з розділовими знаками
         self.assertEqual(rot13('!@#$%'), '!@#$%')
 
-
     def test_rot13_with_punctuation(self):
         # тест на текст з розділовими знаками
         self.assertEqual(rot13('Hello, World!'), 'Uryyb, Jbeyq!')
 
-
     def test_rot13_with_empty_string(self):
-         # тест на порожній рядок
+        # тест на порожній рядок
         self.assertEqual(rot13(''), '')
 
     def test_rot13_with_long_text(self):
-      # тест на обробку довгого тексту
-      long_text = 'A' * 10000
-      expected_output = 'N' * 10000
-      self.assertEqual(rot13(long_text), expected_output)
+        # тест на обробку довгого тексту
+        long_text = 'A' * 10000
+        expected_output = 'N' * 10000
+        self.assertEqual(rot13(long_text), expected_output)
 
     def test_main_valid_input(self):
         # тестуємо поведінку функції main() при отриманні валідного вводу
-     expected_output = f"Введіть текст для шифрування та натисніть Enter: Зашифрований текст: {self.expected_output}\n"
-     with StringIO(self.input_text) as fake_input, StringIO() as fake_output:
-        sys.stdin = fake_input
-        sys.stdout = fake_output
-        main()
-        self.assertEqual(fake_output.getvalue(), expected_output)
-
+        expected_output = self.expected_output
+        with StringIO(self.input_text) as fake_input, StringIO() as fake_output:
+            sys.stdin = fake_input
+            sys.stdout = fake_output
+            main()
+            self.assertEqual(fake_output.getvalue(), expected_output)
 
     def test_main_invalid_input(self):
         # тестуємо поведінку функції main() при отриманні невалідного вводу.
-     expected_output = "Введіть текст для шифрування та натисніть Enter: Зашифрований текст: " + self.expected_output + '\n'
-     with StringIO(self.input_text) as fake_input, StringIO() as fake_output:
-        sys.stdin = fake_input
-        sys.stdout = fake_output
-        main()
-        self.assertEqual(fake_output.getvalue(), expected_output)
+        expected_output = self.expected_output
+        with StringIO(self.input_text) as fake_input, StringIO() as fake_output:
+            sys.stdin = fake_input
+            sys.stdout = fake_output
+            main()
+            self.assertEqual(fake_output.getvalue(), expected_output)
+
+    def test_error_message_in_stderr(self):
+         # тест, що помилка пишеться в stderr
+         with StringIO('1234') as fake_input, StringIO() as fake_error:
+            sys.stdin = fake_input
+            sys.stderr = fake_error
+            try:
+                main()
+            except SystemExit:
+                pass
+            self.assertIn("Помилка: Вхідні данні можуть містити тільки ASCII символи.\n", fake_error.getvalue())
+
+    def test_exit_code_on_error(self):
+        # тест на коди завершення
+        with StringIO('1234') as fake_input, StringIO() as fake_error:
+            sys.stdin = fake_input
+            sys.stderr = fake_error
+            with self.assertRaises(SystemExit) as cm:
+                main()
+            self.assertEqual(cm.exception.code, 1)
+            self.assertIn("Помилка: Вхідні данні можуть містити тільки ASCII символи.\n", fake_error.getvalue())
+
 
 if __name__ == "__main__":
     unittest.main()
