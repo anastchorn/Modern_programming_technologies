ba7e66909941932b125f45497d4ca23761665d96
anastchorn <anastchorn-ipt27@lll.kpi.ua>
2024-05-05 22:47:27 +0300

    second commit

--- Lab3_C/tests/MyClassTests.cpp
+++ Lab3_C/tests/MyClassTests.cpp
@@ -2,24 +2,24 @@
 #include "catch_amalgamated.hpp"
 #include "MyClass.h"
 
-TEST_CASE("Передача за значенням не змінює оригінальний об'єкт", "[value_type]") {
+TEST_CASE("Passing by value does not change the original object", "[value_type]") {
     MyClass obj(1);
     modifyByValue(obj);
     REQUIRE(obj.value == 1);
 }
 
-TEST_CASE("Передача посиланням змінює оригінальний об'єкт", "[reference_type]") {
+TEST_CASE("Passing by reference changes the original object", "[reference_type]") {
     MyClass obj(1);
     modifyByReference(obj);
     REQUIRE(obj.value == 200);
 }
 
-TEST_CASE("Об'єкти створені на стеку знищуються автоматично", "[stack]") {
+TEST_CASE("Objects created on the stack are destroyed automatically", "[stack]") {
     MyClass obj = createOnStack(10);
     REQUIRE(obj.value == 10);
 }
 
-TEST_CASE("Об'єкти створені на хіпі не знищуються автоматично", "[heap]") {
+TEST_CASE("Objects created on a hive are not automatically destroyed", "[heap]") {
     MyClass* obj = createOnHeap(20);
     REQUIRE(obj->value == 20);
     delete obj;
